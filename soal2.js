b0f0048d6c8536f671dc3f295add40ce8b3889ea
sleepy81k <nabilnico@gmail.com>
2020-09-18 05:27:25 +0700

    first

---
+++ soal2.js
@@ -0,0 +1,25 @@
+var kodeUnix = [
+  23,
+  89,
+  67,
+  29,
+  192,
+  6,
+  2,
+  129,
+  21,
+  872,
+  891,
+  901,
+  70,
+  61,
+  78,
+  62,
+  32,
+  90,
+  90,
+];
+
+const codeUnix = kodeUnix.filter((a) => a >= 60);
+
+console.log(codeUnix);
